582cfe9dd10ae8cb2d6996342e34b6805bd32c63
Eric Bilodeau <155914787+CodeNinjasPortCredit@users.noreply.github.com>
2025-04-26 14:00:22 -0400

    worked on touch screen input problems
    made interact only usable while no other inputs are active

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -6,16 +6,37 @@ public class PlayerInteraction : MonoBehaviour
 {
     [SerializeField] private Button interactButton;
     private InputAction interactAction;
+    private InputAction movementAction;
+    private InputAction inventoryAction;
 
+    private bool isInteractingWithInventory = false;
 
     void Awake()
     {
+        // Get the actions from the Input System
         interactAction = InputSystem.actions.FindAction("Interact");
+        movementAction = InputSystem.actions.FindAction("Movement");
+        inventoryAction = InputSystem.actions.FindAction("Inventory");
     }
 
     void Update()
     {
-        if (interactAction.IsPressed())
+        // Check if player is moving or interacting with inventory
+        bool isMoving = movementAction.ReadValue<Vector2>().magnitude > 0;
+        bool isInventoryOpen = inventoryAction.IsPressed();
+
+        // Disable interact action if player is moving or interacting with inventory
+        if (isMoving || isInventoryOpen)
+        {
+            interactAction.Disable();
+        }
+        else
+        {
+            interactAction.Enable();
+        }
+
+        // Only check for interact when interact action is enabled
+        if (interactAction.enabled && interactAction.IsPressed())
         {
             Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 2f);
             foreach (Collider2D collider in colliders)
@@ -23,29 +44,11 @@ public class PlayerInteraction : MonoBehaviour
                 Interactable interactable = collider.GetComponent<Interactable>();
                 if (interactable != null)
                 {
-                    Debug.Log("innit interacted");
+                    Debug.Log("Interacted with " + interactable.name);
                     interactable.Interact();
                     break;
                 }
             }
         }
     }
-
-    /*void Update()
-    {
-        if (interactInnit.triggered);
-        {
-            // Get all colliders in trigger range
-            Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 2f);
-            foreach (Collider2D collider in colliders)
-            {
-                Interactable interactable = collider.GetComponent<Interactable>();
-                if (interactable != null)
-                {
-                    interactable.Interact();
-                    break;
-                }
-            }
-        }
-    }*/
-} 
+}
